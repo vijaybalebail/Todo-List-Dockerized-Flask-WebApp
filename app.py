6e1b0fd425af3bb307e913c7c8c89b6c9d189151
Vijay Balebail <63021453+vijaybalebail@users.noreply.github.com>
2021-08-09 16:24:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
 import os
+import cx_Oracle
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/db.sqlite'
+dnsStr = 'Hello'
+dnsStr = cx_Oracle.makedsn('Hostname', '1521', 'orclpdb')
+dnsStr = dnsStr.replace('SID', 'SERVICE_NAME')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'oracle://ssb:Ora_DB4U@' + dnsStr
+
+
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -11,7 +20,7 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(1000), nullable=False)
-    complete = db.Column(db.Boolean) 
+    complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer)
 
 
@@ -26,7 +35,7 @@ def index():
 # add a task
 @app.route('/add', methods=["POST"])
 def add():
-    
+
     # get the title of the task
     title = request.form.get("title")
 
@@ -35,14 +44,17 @@ def add():
         return redirect(url_for("index"))
     # create a todo object
     newTask = Todo(task=title, complete=False)
-    
+
     # try to add the object to the database
     try:
         db.session.add(newTask)
         db.session.commit()
         return redirect(url_for("index"))
-    except:
-        return "There was an issue adding your task."
+    #except:
+    except exc.SQLAlchemyError as e:
+        print(type(e))
+        error = str(e.__dict__['orig'])
+        return error
 
 
 # delete a task
@@ -51,7 +63,7 @@ def delete(todo_id):
 
     # get the task from the data base
     task = Todo.query.filter_by(id=todo_id).first()
-    
+
     try:
         db.session.delete(task)
         db.session.commit()
@@ -78,7 +90,15 @@ def update(todo_id):
 
 
 if __name__ == "__main__":
-    
+
+    connection = cx_Oracle.connect("SSB", "Ora_DB4U", "129.146.115.121/orclpdb")
+
+    cursor = connection.cursor()
+    cursor.execute("select systimestamp from dual")
+    r, = cursor.fetchone()
+    print(r)
+    print(' is the time')
+
     db.create_all()
     port = int(os.environ.get('PORT', 5000))
 
